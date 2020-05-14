a03d36e19dc017668348d0766f963fe7afc65e63
RandyAn051 <64591674+RandyAn051@users.noreply.github.com>
2020-05-15 06:21:55 +0700

    Initial commit

---
+++ app/src/main/java/com/bapercoding/simplecrud/ApiEndPoint.kt
@@ -0,0 +1,14 @@
+package com.bapercoding.simplecrud
+
+class ApiEndPoint {
+
+    companion object {
+
+        private val SERVER = "http://192.168.1.37/API_nya/"
+        val CREATE = SERVER+"create.php"
+        val READ = SERVER+"read.php"
+        val DELETE = SERVER+"delete.php"
+        val UPDATE = SERVER+"update.php"
+
+    }
+}
